f5985c5995a9d08cb2a24ac0c71bc6404254359f
Gao Yang <gaoyang@visenze.com>
2015-10-22 15:09:43 +0800

    Update bp-phab-dev-setup.py
    corrected GITHUB_RAW_URL

--- bin/bp-phab-dev-setup.py
+++ bin/bp-phab-dev-setup.py
@@ -7,7 +7,7 @@ import sys
 import urllib
 import subprocess
 
-GITHUB_RAW_URL = 'https://github.com/flyingbag/phabricator-setup/master'
+GITHUB_RAW_URL = 'https://raw.githubusercontent.com/flyingbag/phabricator-setup/master'
 
 GITHOOKS = {
     'pre-commit':         '%s/git-hooks/pre-commit',
